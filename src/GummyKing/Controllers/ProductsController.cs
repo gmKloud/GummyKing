03ef64ab2c418c0f39dac68bc5626402d62604ea
Grinil Khanna <gmkhanna@gmail.com>
2017-04-21 13:49:01 -0700

    Add Home and Controllers with CRUD on products. Need to build view pages.

---
+++ src/GummyKing/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GummyKing.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummyKing.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private GummyKingDbContext db = new GummyKingDbContext();
+        
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        //Get - Product Details
+        public IActionResult Details(int id)
+        {
+            var thisProd = db.Products.FirstOrDefault(p => p.ProductId == id);
+            return View(thisProd);
+        }
+
+        //Get - Products Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Post - Products Create
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Get - Update Products
+        public IActionResult Update(int id)
+        {
+            var thisProd = db.Products.FirstOrDefault(p => p.ProductId == -id);
+            return View(thisProd);
+        }
+
+        //POST - Update Products
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Get Delete Product
+        public IActionResult Delete(int id)
+        {
+            var thisProd = db.Products.FirstOrDefault(p => p.ProductId == id);
+            return (thisProd);
+        }
+
+        //POST - Delete Product
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCondfirmed(int id)
+        {
+            var thisProd = db.Products.FirstOrDefault(p => p.ProductId == id);
+            db.Products.Remove(thisProd);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
